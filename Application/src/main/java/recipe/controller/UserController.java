495d048d524f62b330947693f3c41c6a65fe757b
Aminm246 <Aminm246@gmail.com>
2024-10-07 08:21:12 -0500

    Add files via upload

---
+++ Application/src/main/java/recipe/controller/UserController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import model.UserModel;
+import view.UserView;
+import model.User;
+import java.util.List;
+
+public class UserController {
+	private UserModel model;
+	private UserView view;
+
+	public UserController(UserModel model, UserView view) {
+		this.model = model;
+		this.view = view;
+	}
+
+	public void addUser(String name, int id) {
+		User newUser = new User(name, id);
+		model.addUser(newUser);
+		view.updateView();
+	}
+
+	public void updateUser(int id, String newName) {
+		User user = model.getUserById(id);
+		if (user != null) {
+			user.setName(newName);
+			model.updateUser(id, user);
+			view.updateView();
+		}
+	}
+
+	public void deleteUser(int id) {
+		model.deleteUser(id);
+		view.updateView();
+	}
+
+	public User getUserById(int id) {
+		return model.getUserById(id);
+	}
+
+	public List<User> getAllUsers() {
+		return model.getAllUsers();
+	}
+
+	public void handleSaveButtonClick() {
+	}
+
+	public void handleCancelButtonClick() {
+	}
+}
