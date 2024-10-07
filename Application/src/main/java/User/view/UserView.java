d260d9aee19246a632a9843495ecb70b76bd1499
Aminm246 <Aminm246@gmail.com>
2024-10-07 09:03:28 -0500

    Add files via upload

---
+++ Application/src/main/java/User/view/UserView.java
@@ -0,0 +1,21 @@
+package user.view;
+
+import user.model.User;
+import java.util.List;
+
+public class UserView {
+    public UserView() {
+    }
+
+    public void updateView() {
+    }
+
+    public void displayUserInfo(User user) {
+    }
+
+    public void showError(String message) {
+    }
+
+    public void updateUserList(List<User> users) {
+    }
+}
