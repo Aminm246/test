b4162afc8b14f5c4f6af42d59dbb21eb84346a1e
NickReiss <wn4123md@go.minnstate.edu>
2024-10-12 12:51:30 -0500

    Ran project

--- Application/src/main/java/edu/metrostate/Main.java
+++ Application/src/main/java/edu/metrostate/Main.java
@@ -10,15 +10,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Hello World");
+        stage.setTitle("Cookbook v0.1");
 
         StackPane root = new StackPane();
 
         Button button = new Button();
-        button.setText("Button");
+        button.setText("Create Recipe");
         root.getChildren().add(button);
 
-        stage.setScene(new Scene(root, 300, 300));
+        stage.setScene(new Scene(root, 1920, 1080));
         stage.show();
     }
 }
